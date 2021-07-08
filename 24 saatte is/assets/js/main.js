b51eb7897475c3cae10de13f60056b1623d92338
unknown <aygunaab@code.edu.az>
2021-07-07 22:40:48 -0700

    footer

--- 24 saatte is/assets/js/main.js
+++ 24 saatte is/assets/js/main.js
@@ -2,4 +2,14 @@ $(".read-more").click(function(){
 	$(".intro").removeClass("content"),
     $(".read-more").css("display","none")
   });
+  $(".scroll").hover(function(){
+    $(this).animate({
+      'scrollLeft':1150
+    },{duration:600,queue:false});
+  },function(){
+   $(this).animate({
+     'scrollLeft':0
+   },{duration:600,queue:false});
+ 
+  });
  
